cb7edb0bc0641223bd2d929775d904eec1bc0bcb
Kacper Sulima <238925@student.pwr.edu.pl>
2020-03-01 00:16:30 +0100

    spaghetti take on creating character

--- GMHelper/GMHelper.cpp
+++ GMHelper/GMHelper.cpp
@@ -13,29 +13,55 @@ public:
 	string name, surname, hairColor, eyesColor;
 	~_NPCBuilder() {};
 	_NPCBuilder() {};
-//	_NPCBuilder(int w, int h, int a, string n, string sn, string hC, string eC) {}
-//	virtual void rollWW(int manRoll);
+
 };
 
 class Dwarf : public _NPCBuilder {
 	int ww = 30, us = 20, k = 20, odp = 30, zr = 10, intel = 20, sw = 20, ogd = 10;
 	int roll = NULL;
 public :
-	Dwarf() {};
-	void rollWW(int manRoll) {
-		srand(time(NULL));
-		if (manRoll == 0) {
-			ww += rand() % 20 + 1;
-		}
-		else
-		{
-			ww += manRoll;
+	
+	Dwarf() {
+		cout << "Podaj wiek swojej postaci: "; cin >> this->age; cout << endl;
+		cout << "Podaj wzrost swojej postaci: "; cin >>	this->height; cout << endl;
+		cout << "Podaj wage swojej postaci: "; cin >> this->weight; cout << endl;
+	};
+	
+
+
+	void setStat(string whichStat) {
+		
+		if (whichStat == "ww" || whichStat == "WW") {
+			int ifRoll = 0;
+			do {
+				cout << "Rzucasz, czy mam to zrobic za Ciebie?" << endl;
+				cout << "Jezeli czujesz siê szczesliwy - po prostu wpisz wynik z Twoich rzutow 2k10. Jezeli chcesz bym to ja losowal - wpisz 0" << endl;
+				cin >> ifRoll;
+				if (ifRoll == 0) {
+					srand(time(NULL));
+					ww += rand() % 20 + 1;
+				}
+				if (ifRoll > 20 || ifRoll < 0) {
+					cout << "Oszukujesz albo mnie, albo siebie - rzut dla krasnoluda wykonujemy 2k10!" << endl;
+				}
+				else {
+					ww += ifRoll;
+				}
+			} while (ifRoll > 20 || ifRoll < 0);
 		}
 	}
 	int getWW() {
 		return ww;
 	}
 };
-
+void testDwarf() {
+	Dwarf d;
+	string stat;
+	cout << "Podaj statystyke do ustawienia" << endl;
+	cin >> stat;
+	d.setStat(stat);
+	cout << d.getWW();
+}
 int main() {
+	testDwarf();
 }
